95c32367b91a935eee56064d5094d0192e4f4d5e
Angelia Horne <AngeliaMHorne@gmail.com>
2023-04-25 15:03:01 -0600

    setting up

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Start of Main
         </a>
       </header>
     </div>
